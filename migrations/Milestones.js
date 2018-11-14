5c8e04678dba76f5449b3b5931cad7a4da732aa7
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-11-14 00:18:13 +0000

    implimentation of knex migrations

---
+++ migrations/Milestones.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('Milestones', function(table) {
+      table.string('description');
+      table.date('date_achieved');
+      table.increments();
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('Milestones')
+  ]);
+};
